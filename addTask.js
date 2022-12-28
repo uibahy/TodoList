fbf7ab2e8c9e5ed7a94b8baadf29a210ad5ef101
bahy <68196786+uibahy@users.noreply.github.com>
2022-12-28 17:52:28 +0100

    Update addTask.js

--- addTask.js
+++ addTask.js
@@ -3,14 +3,23 @@ let tasks = [
         title: "first",
         date: "12/12/12",
         completed: false,   
-        id: 0,
+    },
+    {
+        title: "first",
+        date: "12/12/12",
+        completed: false,   
+    },
+    {
+        title: "first",
+        date: "12/12/12",
+        completed: false,   
     }
 ]
 
-// tasks = JSON.parse(localStorage.getItem("tasks"))
-
+tasks = JSON.parse(localStorage.getItem("tasks"))
 function taskForm(){
     document.getElementById("todoList").innerHTML = ""
+    let taskIndex = 0
     for (task of tasks){
 
         let taskFormat =
@@ -23,13 +32,14 @@ function taskForm(){
                     </div>
         
                     <div class="icons">
-                        <div id="editTodo" onclick="editTask(${task.id})" class="edit"><img src="/edit.svg" alt=""></div>
-                        <div id="removeBtn" onclick="deleteTask(${task.id})" class="delete"><img src="/trash.svg" alt=""></div>
-                        <div ondblclick="undoTask(${task.id})" id="doneTask" onclick="doneTask(${task.id})" class="done"><img src="/trash-true.svg" alt=""></div>
+                        <div id="editTodo" onclick="editTask(${taskIndex})" class="edit"><img src="/edit.svg" alt=""></div>
+                        <div id="removeBtn" onclick="deleteTask(${taskIndex})" class="delete"><img src="/trash.svg" alt=""></div>
+                        <div ondblclick="undoTask(${taskIndex})" id="doneTask" onclick="doneTask(${task.id})" class="done"><img src="/trash-true.svg" alt=""></div>
                     </div>
                 </div>
             `
         document.getElementById("todoList").innerHTML += taskFormat
+        taskIndex++
 
     }
 }
@@ -37,45 +47,52 @@ function taskForm(){
 taskForm()
 
 function addTask(){
+    let inputValue = document.getElementById("todoInput").value
     let newTask = {
-        title: document.getElementById("todoInput").value,
+        title: inputValue,
         date: new Date().toLocaleString(),
         completed: false,   
-        id: task.id + 1,
     }
-    tasks.push(newTask)
+
+    if (inputValue.length == 0){
+        alert("fuck off fnr")
+    } else {
+        tasks.push(newTask)
+    }
+    document.getElementById("todoInput").value = ""
     localSt()
     taskForm()
 }
 
-function editTask(taskId){
-    if (tasks[taskId].completed == true){
+function editTask(taskIndex){
+    if (tasks[taskIndex].completed == true){
         alert("you can't")
     } else {
         let newTaskTitle = prompt("edit")
         if (newTaskTitle != null) { 
-            tasks[taskId].title = newTaskTitle
-            tasks[taskId].date = `Edited at ${new Date().toLocaleString()}`     
+            tasks[taskIndex].title = newTaskTitle
+            tasks[taskIndex].date = `Edited at ${new Date().toLocaleString()}`     
         }
     }
     localSt()
     taskForm()
 }
 
-function deleteTask(taskId){
-    tasks.splice(taskId, 1)
+function deleteTask(taskIndex){
+    // alert(taskIndex)
+    tasks.splice(taskIndex, 1)
     localSt()
     taskForm()
 }
 
-function doneTask(taskId){
-    tasks[taskId].completed = true
+function doneTask(taskIndex){
+    tasks[taskIndex].completed = true
     localSt()
     taskForm()
 }
 
-function undoTask(taskId){
-    tasks[taskId].completed = false
+function undoTask(taskIndex){
+    tasks[taskIndex].completed = false
     localSt()
     taskForm()
 }
